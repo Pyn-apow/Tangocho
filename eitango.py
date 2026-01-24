9c3708ed774bbf6f95fc276c3b6010a5de7842ac
Pynapow <ninninninningen@gmail.com>
2026-01-24 17:30:44 +0900

    Initial commit

---
+++ eitango.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+# CSV読み込み
+df = pd.read_csv("tangocho.csv")
+
+WORDS_PER_TEST = 100
+
+total_words = len(df)
+num_tests = (total_words - 1) // WORDS_PER_TEST + 1
+
+print(f"全{total_words}語 / {num_tests}セットあります")
+
+# どの100語をやるか選択
+test_no = int(input(f"何セット目をやりますか？ (1～{num_tests})：")) - 1
+
+start = test_no * WORDS_PER_TEST
+end = start + WORDS_PER_TEST
+test_df = df.iloc[start:end]
+
+correct = 0
+
+print("\n--- 単語テスト開始 ---\n")
+1
+for i, row in test_df.iterrows():
+    jp = row["jp"]
+    en = row["en"]
+
+    answer = input(f"{jp}({en[0]}-)：").strip()
+
+    if answer.lower() == en.lower():
+        print("○ 正解\n")
+        correct += 1
+    else:
+        print(f"× 不正解（正解：{en}）\n")
+
+print("=== 結果 ===")
+print(f"{len(test_df)}問中 {correct}問 正解")
+print(f"正答率：{correct / len(test_df) * 100:.1f}%")
+
+
+
